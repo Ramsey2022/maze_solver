d3891a85046ca7970e66e3be784e45d99b4fb711
Ramsey2022 <abovethelotus@yahoo.com>
2023-11-27 11:23:50 -0800

    imported pyamaze to create random mazes

--- solver.py
+++ solver.py
@@ -1,1 +1,11 @@
 from pyamaze import maze
+m=maze(5, 5)
+m.CreateMaze()
+
+#Creates dictionary with the maze cells as keys and values as another dictionary of (N)orth (S)outh (E)ast (W)est. 1 means path is open, 0 means path is closed
+print(m.maze_map)
+
+#Creates a list of all cells
+print(m.grid)
+
+m.run()
