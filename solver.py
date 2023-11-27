269155d154846106422d4ef9ab074195d104091e
Ramsey2022 <abovethelotus@yahoo.com>
2023-11-27 11:29:04 -0800

    added heuristic function

--- solver.py
+++ solver.py
@@ -1,4 +1,12 @@
 from pyamaze import maze
+
+#Heuristic function finding manhattan distance between two cells
+def h(cell1, cell2):
+    x1,y1 = cell1
+    x2,y2 = cell2
+    
+    return abs(x1 - x2) + abs(y1 - y2)
+
 m=maze(5, 5)
 m.CreateMaze()
 
